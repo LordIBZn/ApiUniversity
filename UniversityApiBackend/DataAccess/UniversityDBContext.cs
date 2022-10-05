ba75e81ff21034e5a4a40169c1817140718c4eee
Edgardo Hiram Ibañez Villalobos <ibzedgarovillalobos@gmail.com>
2022-10-04 22:28:10 -0700

    Add project files.

---
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
+        {
+
+        }
+
+        //ADD Dbset (Tables of out Data base)
+        public DbSet<User>? Users { get; set; }  
+        public DbSet<Course>? Courses { get; set; }
+    }
+}
---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required, StringLength (500)]
+        public string LongDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string TargetAudiences { get; set; } = string.Empty;
+
+        [Required]
+        public string Objectives { get; set; } = string.Empty;
+
+        [Required]
+        public string Requirements { get; set; } = string.Empty;
+
+        [Required]
+        public virtual string LevelTypeAsString
+        {
+            get
+            {
+                return this.levelType.ToString();
+            }
+            set
+            {
+                levelType = (levelTypes)Enum.Parse(typeof(levelTypes), value, true);
+            }
+        }
+        public levelTypes levelType { get; set; }
+
+        public enum levelTypes
+        {
+            Basic = 1,
+            Intermediate = 2,
+            Advance = 3
+        }
+    }
+}
